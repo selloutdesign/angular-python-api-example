b784aa8503887a669fa122c0374795769daad8a5
Josh <j.selloutdesign@gmail.com>
2017-08-16 11:55:53 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from  '@angular/router';
+import { JsonpModule } from '@angular/http';
+import {NgxPaginationModule} from 'ngx-pagination'; 
+
 
 import { AppComponent } from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { JobService } from './job.service';
+
+import { AppRoutingModule } from './app-routing.module';
+import { JLoaderComponent } from './j-loader/j-loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent,
+    JLoaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    JsonpModule,
+    NgxPaginationModule
   ],
-  providers: [],
+  providers: [HeroService, JobService],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
